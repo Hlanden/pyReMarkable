95497b2f2efae547ad00f81764343a355d898815
Hlanden <jorghaal@stud.ntnu.no>
2020-11-04 13:49:24 +0100

    Add keyring manager for rm-code and device-id

--- client.py
+++ client.py
@@ -7,26 +7,62 @@
 
 import requests
 import uuid
+import keyring
 
 class Client:
-    """Class for authorizing and requesting towards the cloud
+    """Class for authorizing and requesting towards the cloud-server
 
     Once the class has authorized the user to the cloud, it will store the bearer token that is needed
     for communicating with the cloud.
     """
-    def __init__(self, rm_code, token=False, device_id=False, device_desc='desktop-Windows'):
+    def __init__(self, rm_code= '', token='', device_id='', device_desc='desktop-Windows'):
         self.token = token
         self.rm_code = rm_code
         self.device_id = device_id
         self.device_desc = device_desc
 
-        if not self.device_id:
-            self.generate_device_id()
+        if not rm_code:
+            if not self.get_token_and_id_from_keyring():
+                print('Please enter a valid RM code')
+            else:
+                print('Successfully got RM-code and device-ID from keyring')
+        else:
+            if not self.get_token_and_id_from_keyring():
+                self.generate_device_id()
+                self.set_new_key_ring()
+            else:
+                print('Successfully got RM-code and device-ID from keyring')
 
         if not self.token:
             self.generate_bearer_token()
         else:
             self.refresh_token()
+
+    def get_token_and_id_from_keyring(self):
+        """Get device id and rm-code from keyring
+
+        :return: True if success else False
+        """
+        credentials = keyring.get_credential('pyRm', None)
+        if credentials is None:
+            print('No credentials set for this computer')
+            return False
+        else:
+            self.device_id = credentials.username
+            self.rm_code = credentials.password
+            return True
+
+    def set_new_key_ring(self):
+        """Create new keyring with device id and rm-code
+
+        :return: True if success else False
+        """
+        if self.device_id and self.rm_code:
+            keyring.set_password('pyRm', self.device_id, self.rm_code)
+            print('New keyring created')
+            return True
+        else:
+            return False
 
     def generate_bearer_token(self):
         """Register this application as new device and generates a new Bearer Authentication token."""
@@ -41,12 +77,12 @@ class Client:
             new_token = str(response.content)
             print('Newly generated token: {}'.format(new_token)) #TODO: Remove
             self.token = new_token
-        return new_token
+            return new_token
 
 
     def generate_device_id(self):
         """ Generates an UUID4 code which can be used as device identification towards the rm."""
-        self.device_id = uuid.uuid4()
+        self.device_id = str(uuid.uuid4())
         print('New UUID4: {}'.format(self.device_id))
 
     def refresh_token(self):
@@ -72,4 +108,6 @@ class Client:
 
 if __name__ == '__main__':
     client = Client('YOUR RM CODE HERE')
+
+
 
